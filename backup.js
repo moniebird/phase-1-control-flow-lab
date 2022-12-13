a3ab998aaf6f10402c8f9eddefc4166206d80064
moniebird <moniebird@mac.com>
2022-12-13 17:12:37 -0500

    commit tree fb3c3d411c352100508e89039827f04e20633eb5

---
+++ backup.js
@@ -0,0 +1,13 @@
+function scuberGreetingForFeet(){
+    let ride = 199
+    if (ride <= 400){
+    return("This one is on me!");
+    }
+    else if (ride >= 2001){
+    return("I will gladly take your thirty bucks.")
+    }
+    else (ride <= 2501);{
+    return("No can do.")
+        }
+  }
+  
